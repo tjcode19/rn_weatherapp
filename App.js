c5bc5b2df5c5f35604e245eb69fe366dd3e0ec7f
Tolulope <tjcode19@gmail.com>
2023-08-14 06:51:48 +0100

    upcoming and currentweather screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import UpcomingWeather from "./src/components/UpcomingWeather";
+
+
 
 export default function App() {
-  return (
+ return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <UpcomingWeather />
     </View>
   );
 }
@@ -13,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/components/UpcomingWeather.js
@@ -0,0 +1,90 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text, SafeAreaView, FlatList, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const DATA = [
+  {
+    dt_txt: "2023-02-18 12:00:00",
+    main: { temp_max: 8.55, temp_min: 7.55 },
+    weather: [{ main: "clear" }],
+  },
+  {
+    dt_txt: "2023-02-19 12:00:00",
+    main: { temp_max: 8.55, temp_min: 7.55 },
+    weather: [{ main: "clouds" }],
+  },
+  {
+    dt_txt: "2023-02-20 12:00:00",
+    main: { temp_max: 8.55, temp_min: 7.55 },
+    weather: [{ main: "rain" }],
+  },
+  {
+    dt_txt: "2023-02-21 12:00:00",
+    main: { temp_max: 8.55, temp_min: 7.55 },
+    weather: [{ main: "sunny" }],
+  },
+];
+
+const Item = (props) => {
+  const { dt_txt, min, max, condition } = props;
+  return (
+    <View style={styles.item}>
+      <Feather name="sun" size={50} color="white" />
+      <Text style={styles.date}>{dt_txt}</Text>
+      <Text style={styles.temp}>{min}</Text>
+      <Text style={styles.temp}>{max}</Text>
+    </View>
+  );
+};
+
+export default function UpcomingWeather() {
+  const renderItem = ({ item }) => {
+    return (
+      <Item
+        condition={item.weather[0].main}
+        dt_txt={item.dt_txt}
+        min={item.main.temp_min}
+        max={item.main.temp_max}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Upcoming Weather</Text>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_txt}
+        ></FlatList>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "red",
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderWidth: 5,
+    backgroundColor: "pink",
+  },
+  temp: {
+    color: "white",
+    fontSize: 20,
+  },
+  date: {
+    color: "white",
+    fontSize: 15,
+  },
+});
